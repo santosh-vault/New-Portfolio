dc59de69e542c7d3121f871cd9bba911eddbcef0
Santosh Subedi <santossubedi33@gmail.com>
2025-02-04 21:22:29 +0545

    about update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,7 @@
 // GlowingBoxWithText.js
-import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer'
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const About = () => {
   return (
@@ -17,48 +17,45 @@ const About = () => {
         </div>
         <div className="bg-purple-800 text-white p-8 md:p-12 lg:p-16 rounded-md relative z-10">
           <div className="text-md lg:text-md leading-relaxed mb-6">
-            Hello, I'm Santosh, a passionate BCA student on the journey to
-            becoming a frontend web developer. Proficient in React JS,
-            Javascript, Tailwind, and CSS, I craft user-friendly websites that
-            leave a lasting impression.
+            Hello, I'm Santosh, a passionate UI/UX and graphic designer driven
+            by the art of creating visually stunning and user-centric digital
+            experiences. Proficient in Figma, Adobe Photoshop, and design
+            principles, I craft designs that captivate and connect.
           </div>
           <div className="text-md lg:text-md leading-relaxed mb-6 italic text-gray-300 w-4/5 mx-auto">
-            Currently immersed in my BCA studies, I seamlessly blend my academic
-            knowledge with a love for learning new things rapidly.
+            Currently immersed in mastering design trends and user behaviors, I
+            seamlessly blend my creativity with a strong technical approach to
+            bring fresh concepts to life.
           </div>
           <div className="text-md lg:text-md leading-relaxed mb-6">
-            Bringing to the table a unique blend of frontend development
-            expertise, creative design skills, and a hunger for continuous
-            learning, I thrive on transforming ideas into digital reality.
+            My approach combines user experience strategies with bold graphic
+            designs, ensuring every project strikes the right balance between
+            aesthetics and functionality.
           </div>
           <div className="text-md lg:text-md leading-relaxed mb-6">
-            With a track record as a freelance web developer, I've successfully
-            collaborated with clients, achieving milestones that showcase my
-            dedication to delivering exceptional results.
+            With a proven track record as a freelance designer, I've
+            successfully collaborated with clients to deliver design solutions
+            that stand out and communicate effectively.
           </div>
           <div className="text-md lg:text-md leading-relaxed mb-6">
-            My current goals include expanding my skill set, embracing new
-            challenges, and fostering collaborations that push boundaries in the
-            digital realm—excited about the prospect of working together and
-            creating something extraordinary.
+            My current goals include mastering emerging design trends,
+            collaborating with like-minded creatives, and pushing boundaries in
+            digital design—excited about the prospect of working together and
+            crafting memorable designs.
           </div>
           <div className="text-md lg:text-md leading-relaxed mb-6">
-            Feel free to contact me at{' '}
+            Feel free to contact me at{" "}
             <a
               href="mailto:santoshsub.33@gmail.com"
               className="text-purple-300 underline"
             >
               santoshsub.33@gmail.com
             </a>
-            . Let's connect and explore the possibilities of innovation and
-            growth!
+            . Let's connect and bring innovative designs to life!
           </div>
         </div>
- 
       </div>
-      
     </div>
-
   );
 };
 
