eaf4c4496f29783c520cad986d166706dd03457b
Santosh Subedi <santossubedi33@gmail.com>
2025-07-12 21:21:52 +0545

    Updated Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ import GraphicsProject from "./GraphicsProject";
 const Hero = () => {
   return (
     <section
-      className="flex flex-col sliding-animation min-h-screen w-full ml-64"
+      className="flex flex-col sliding-animation min-h-screen w-full ml-56"
       style={{ backgroundColor: "#0b0b0b", minHeight: "100vh" }}
     >
       <div className="flex-1">
